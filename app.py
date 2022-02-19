7152b3de4efe5feb8708587ec89a72095dfacd70
Tobias Eiter <tobias.eiter@student.ubik.ac.at>
2022-02-19 04:09:21 +0100

    added views

--- app.py
+++ app.py
@@ -19,21 +19,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Test(db.Model):
-    __tablename__ = 'test'
+class Player(db.Model):
+    __tablename__ = 'player'
     id = db.Column(db.Integer, primary_key=True)
-    tester = db.Column(db.String(200), unique=True)
+    first_name = db.Column(db.String(200), unique=True)
+    last_name = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(200), unique=True)
+    shirt_number = db.Column(db.String(200), unique=True)
+    email = db.Column(db.String(200), unique=True)
 
-    def __init__(self, tester):
-        self.tester = tester
+    def __init__(self, first_name, last_name, password, shirt_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.shirt_number = shirt_number
+        self.email = email
 
 
-#db.create_all()
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+if dev:
+    db.create_all()
 
 
 if __name__ == '__main__':
