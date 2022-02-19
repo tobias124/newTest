934e633c5c08906aa6cc424cf7f068462f3edaee
Tobias Eiter <tobias.eiter@student.ubik.ac.at>
2022-02-19 03:00:45 +0100

    init commit

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-# app.py
 from flask import Flask
-app = Flask(__name__)
 
-from model import *
-@app.teardown_appcontext
-def shutdown_session(exception=None):  # exception=None is needed
-    db_session.remove()
+app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    q = User.query.filter(User.username == 'guest').first()
-    return 'Hello, {}! Your username is {} and password is {}'.format(q.name, q.username, q.password)
+    return 'Hello, World!'
 
 
 if __name__ == '__main__':
