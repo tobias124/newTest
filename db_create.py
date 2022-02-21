eae5653fda5ad5cbaf19dcebdf3feba174b69a38
Tobias Eiter <tobias.eiter@student.ubik.ac.at>
2022-02-21 01:55:23 +0100

    added bet, games

--- db_create.py
+++ db_create.py
@@ -1,2 +1,10 @@
 from website import db
+from website.models import Player
+from werkzeug.security import generate_password_hash, check_password_hash
+
 db.create_all()
+new_player = Player(email="biasi_eiter@hotmail.com", first_name="Tobias", last_name="Eiter", 
+shirt_number=17, password=generate_password_hash("1234", method='sha256'),
+role="ADMIN")
+db.session.add(new_player)
+db.session.commit()
